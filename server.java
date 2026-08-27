7763831fb04028e946e438920ada92c4959ace15
bycarolina <csilveira2612@gmail.com>
2026-08-27 19:42:42 -0300

    feat: add first test

--- server.java
+++ server.java
@@ -31,6 +31,40 @@ public class Server {
                     "Cliente conectado: " + cliente.getInetAddress()
             );
 
+            BufferedReader leitor = new BufferedReader(
+                    new InputStreamReader(cliente.getInputStream())
+            );
+
+            String requisicao = leitor.readLine();
+
+            System.out.println("Requisição recebida: " + requisicao);
+
+            String[] partes = requisicao.split(" ", 2);
+
+            if (partes.length != 2 || !partes[0].equals("GET")) {
+                System.out.println("Requisição inválida.");
+                cliente.close();
+                serverSocket.close();
+                return;
+            }
+
+            String nomeArquivo = partes[1];
+
+            File arquivo = new File("arquivos", nomeArquivo);
+
+            if (arquivo.exists() && arquivo.isFile()) {
+                System.out.println(
+                        "Arquivo encontrado: " + nomeArquivo
+                );
+            } else {
+                System.out.println(
+                        "Arquivo não encontrado: " + nomeArquivo
+                );
+            }
+
+            cliente.close();
+            serverSocket.close();
+
         } catch (IOException e) {
             System.out.println(
                     "Erro no servidor: " + e.getMessage()
